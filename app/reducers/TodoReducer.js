46c2c6d6b9749d35d71848b09ef4229e7f26f6a2
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-09-05 10:50:14 +0700

    Test render

--- app/reducers/TodoReducer.js
+++ app/reducers/TodoReducer.js
@@ -1,41 +1,50 @@
-import { CreateStore } from 'redux'
+// const todo = (state = 0, action) =>{
+//     switch (action.type) {
+//         case 'ADD_TODO':
+//             return {
+//                 id: action.id,
+//                 title: action.title,
+//                 checked: action.checked
+//             }
+//         case 'TOGGLE_TODO':
+//             if(state.id !== action.id){
+//                 return state
+//             }
+//
+//             return {
+//                 ...state,
+//                 checked: !state.checked
+//             }
+//         default:
+//             return state
+//     }
+// };
+//
+// const todos = (state = [], action) => {
+//     switch (action.type) {
+//         case 'ADD_TODO':
+//             return [
+//                 ...state,
+//                 todo(undefined, action)
+//             ]
+//         case 'TOGGLE_TODO':
+//             return state.map(t =>
+//                 todo(t, action)
+//             )
+//         default:
+//             return state
+//     }
+// };
 
-const todo = (state = 0, action) =>{
-    switch (action.type) {
-        case 'ADD_TODO':
-            return {
-                id: action.id,
-                title: action.title,
-                checked: action.checked
-            }
-        case 'TOGGLE_TODO':
-            if(state.id !== action.id){
-                return state
-            }
-
-            return {
-                ...state,
-                checked: !state.checked
-            }
+const counter = (state = 0, action) => {
+    switch (action.type){
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
         default:
-            return state
-    }
-};
-
-const todos = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                todo(undefined, action)
-            ]
-        case 'TOGGLE_TODO':
-            return state.map(t =>
-                todo(t, action)
-            )
-        default:
-            return state
+            return state;
     }
 }
 
-export default todos
+export default counter
--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
+import TodoReducer from './app/reducers/TodoReducer'
+import {createStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const Counter = ({value, onIncrement, onDecrement}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+);
+
+const store = createStore(TodoReducer);
+
+const render = () => {
+    ReactDOM.render(<Counter
+        value={store.getState()}
+        onIncrement={() =>
+        store.dispatch({
+            type: 'INCREMENT'
+        })
+    }
+        onDecrement={() =>
+        store.dispatch({
+            type: 'DECREMENT'
+        })
+    }
+    />, document.getElementById('app'))
+};
+
+render();
+
+store.subscribe(render);
